0551ea7d1cae8bfd92c6377a3982a5f83fefe45a
Egor Litvinov <liveegor@yandex.ru>
2015-05-08 18:55:23 +0400

    blabla

---
+++ mapn.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 08 18:37:15 2015
+
+@author: egor
+"""
+
+from mapnWindow import Ui_mapnWindow
+from PyQt4 import QtCore, QtGui 
+
+class Mapn (QtGui.QMainWindow):
+    def __init__(self):
+        pass
